f5d9edd31f36cef7893cddd889fc47a0f692286b
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2024-01-03 23:47:42 +0530

    added logout route

---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const passport = require('passport');
+
+const { logout } = require('../controllers/auth');
+
+const router = express.Router();
+
+router.get(
+  '/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }),
+);
+
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  },
+);
+
+router.get('/logout', logout);
+
+module.exports = router;
