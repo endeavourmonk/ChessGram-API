399da9a355c67b9ecb018a7310930f6af3e2fc2d
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2024-01-03 23:48:12 +0530

    updated base route for api

--- app.js
+++ app.js
@@ -1,10 +1,52 @@
 const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+
+const authRouter = require('./routes/auth');
+const userRouter = require('./routes/users');
+const homeRouter = require('./routes/home');
+
+const passportSetup = require('./config/passport');
+
 const app = express();
 
-const router = express.Router();
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// express session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { secure: true },
+  }),
+);
+
+// Initialize Passport and restore authentication state, if any, from the session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Parse incoming requests with JSON payloads.
+app.use(express.json({ limit: '10kb' }));
+
+app.get('/', homeRouter);
+app.use('/users', userRouter);
+app.use('/auth', authRouter);
+
+// if none of the above routes matched
+app.all('*', (req, res, next) => {
+  next(`404 ${req.originalUrl} not found`);
+});
+
+// Global error Handling
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(500).json({
+    message: 'something went wrong',
+    error: err,
+  });
 });
 
 module.exports = app;
