8b08234d0a8009042b8e02dfc1c71d0906886289
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2024-01-18 01:26:08 +0530

    modified files

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use(
       maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
       domain: 'chessgram-api.onrender.com',
       path: '/', // Match protected routes' path
+      sameSite: 'none', // Allow cross-origin requests
     },
   }),
 );
